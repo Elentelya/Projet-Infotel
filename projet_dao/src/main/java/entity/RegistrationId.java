f5cb9509525e0211760747dba4e38e1c15f3540b
Maeva <maeva.gaultier.35@gmail.com>
2018-03-05 10:33:51 +0100

    Intégration bdd ok

--- projet_dao/src/main/java/entity/RegistrationId.java
+++ projet_dao/src/main/java/entity/RegistrationId.java
@@ -4,7 +4,7 @@ import javax.persistence.Column;
 import javax.persistence.Embeddable;
 
 @Embeddable
-public class RegistrationId {
+public class RegistrationId implements java.io.Serializable {
 	
 	 @Column(name = "library")
 	    private int registrationLibrary;
